30f9f0f99a820ec33a076a35607d9a57a23ce034
erral <mlarreategi@codesyntax.com>
2011-12-12 08:28:19 +0000

    initial version
    svn path=/cs.linguaplone.links/trunk/; revision=52819

---
+++ README.txt
@@ -0,0 +1,12 @@
+Introduction
+============
+
+This products supports explicit annotations for web pages rendering same content in
+different languages as suggested by Google here:
+
+  http://googlewebmastercentral.blogspot.com/2010/09/unifying-content-under-multilingual.html 
+
+  http://googlewebmastercentral.blogspot.com/2011/12/new-markup-for-multilingual-content.html
+
+It just adds a viewlet with the links to all translated pages of the page you are viewing. You
+won't see anything on your site, it's just a markup for crawlers.
---
+++ setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup, find_packages
+import os
+
+version = '1.0'
+
+setup(name='cs.linguaplone.links',
+      version=version,
+      description="A viewlet to write links at HTML <head> tag with links to translations",
+      long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      # Get more strings from
+      # http://pypi.python.org/pypi?:action=list_classifiers
+      classifiers=[
+        "Framework :: Plone",
+        "Programming Language :: Python",
+        ],
+      keywords='plone linguaplone i18n links',
+      author='Mikel Larreategi',
+      author_email='mlarreategi@codesyntax.com',
+      url='http://code.codesyntax.com/private/cs.linguaplone.links/',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['cs', 'cs.linguaplone'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,
+      )
